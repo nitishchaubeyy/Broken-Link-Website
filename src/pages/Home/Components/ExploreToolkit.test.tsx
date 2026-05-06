e95e0d927cc01bd259e4d9549d424d147bfb0e4a
Tamir Abutbul <1tamir198@gmail.com>
2026-05-07 00:33:56 +0300

    fix test not working (#475)

--- src/pages/Home/Components/ExploreToolkit.test.tsx
+++ src/pages/Home/Components/ExploreToolkit.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@test-utils';
 import { i18n } from 'i18next';
+import { MemoryRouter } from 'react-router-dom';
 import { vi } from 'vitest';
 import { ExploreToolkit } from '@/pages/Home/Components/ExploreToolkit';
 
@@ -11,27 +12,28 @@ vi.mock('react-i18next', () => ({
 }));
 
 describe('ExploreToolkit component', () => {
+  const renderExploreToolkit = () =>
+    render(
+      <MemoryRouter>
+        <ExploreToolkit />
+      </MemoryRouter>
+    );
+
   it('renders the title and description correctly', async () => {
-    render(<ExploreToolkit />);
-    expect(
-      await screen.findByRole('heading', { level: 1, name: /explore_toolkit_title/i })
-    ).toBeInTheDocument();
+    renderExploreToolkit();
+    expect(await screen.findByText(/explore_toolkit_title_part1/i)).toBeInTheDocument();
+    expect(await screen.findByText(/explore_toolkit_title_highlight/i)).toBeInTheDocument();
+    expect(await screen.findByText(/explore_toolkit_title_part2/i)).toBeInTheDocument();
     expect(await screen.findByText(/explore_toolkit_description/i)).toBeInTheDocument();
   });
 
   it('renders three cards with their titles and descriptions', async () => {
-    render(<ExploreToolkit />);
-    expect(
-      await screen.findByRole('heading', { level: 3, name: /explore_toolkit_card1_title/i })
-    ).toBeInTheDocument();
+    renderExploreToolkit();
+    expect(await screen.findByText(/explore_toolkit_card1_title/i)).toBeInTheDocument();
     expect(await screen.findByText(/explore_toolkit_card1_description/i)).toBeInTheDocument();
-    expect(
-      await screen.findByRole('heading', { level: 3, name: /explore_toolkit_card2_title/i })
-    ).toBeInTheDocument();
+    expect(await screen.findByText(/explore_toolkit_card2_title/i)).toBeInTheDocument();
     expect(await screen.findByText(/explore_toolkit_card2_description/i)).toBeInTheDocument();
-    expect(
-      await screen.findByRole('heading', { level: 3, name: /explore_toolkit_card3_title/i })
-    ).toBeInTheDocument();
+    expect(await screen.findByText(/explore_toolkit_card3_title/i)).toBeInTheDocument();
     expect(await screen.findByText(/explore_toolkit_card3_description/i)).toBeInTheDocument();
   });
 });
